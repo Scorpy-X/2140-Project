99490e270a0fcc0b05ae673e6493c056e7ce2f11
Scorpy-X <ronhugh46@gmail.com>
2024-11-22 14:51:11 -0500

    Made furniture class

---
+++ src/Furniture.java
@@ -0,0 +1,53 @@
+
+public class Furniture(){
+
+    private String type;
+    private String state;
+
+
+    public Furniture(){}
+
+    public Furniture(String type, String state){
+
+        this.type = type;
+        this.state = state;
+
+    }
+
+    //Accessors 
+    public String getType() {
+        return type;
+    }
+
+
+    public String getState() {
+        return state;
+    }
+
+    public void setState(String state) {
+        this.state = state;
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
