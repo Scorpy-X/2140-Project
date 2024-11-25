0e680ab8091349ac91758aaeacd2696563deafe0
Justin-06-Watts <jahwatts6@gmail.com>
2024-11-25 11:01:40 -0500

    Additional DataManager methods
    Added two more methods to add and update furniture in the database

--- src/DataManager.java
+++ src/DataManager.java
@@ -31,8 +31,7 @@ public class DataManager {
 					CREATE TABLE IF NOT EXISTS Furniture(
 					id_num INTEGER PRIMARY KEY,
 					type TEXT,
-					condition TEXT,
-					description TEXT,
+					state TEXT,
 					room_id Text,
 					FOREIGN KEY (room_id) REFERENCES Rooms (r_number))""";
 			stmt.execute(query);
@@ -45,11 +44,17 @@ public class DataManager {
 			System.out.println("An error occurred: "+e.getMessage());
 		}
 	}
-	public static void insertOccupant(Occupant p){ //Inserts new occupant information into the occupant table
+	public static void insertOccupant(Occupant p, String room_id){ //Inserts new occupant information into the occupant table
 		String url= "jdbc:sqlite:Room_Information.db";
 		try(Connection conn = DriverManager.getConnection(url);
 			Statement stmt = conn.createStatement()){
-			String query = "INSERT INTO Occupant (idNum,fname,phoneNumber,email,room_id) VALUES ("+p.getIdNum()+",'"+p.getfName()+"','"+p.getPhoneNumber()+"','"+p.getEmail()+"',1"+")"; //Missing Room ID Parameter
+			String query = "INSERT INTO Occupant (idNum,fname,lname,phoneNumber,email,room_id) VALUES ("
+							+p.getIdNum()+",'"
+							+p.getfName()+"','"
+							+p.getlName()+"','"
+							+p.getPhoneNumber()+"','"
+							+p.getEmail()+"','"
+							+room_id+"')";
 			stmt.execute(query);
 			System.out.println("Insert Successful");
 		}catch (SQLException e) {
@@ -62,17 +67,23 @@ public class DataManager {
 	}
 	
 	
-	public static void updateOccupant(long id, Occupant new_p){//Updates an existing record in the Occupant Table
+	public static void updateOccupant(long id, Occupant new_p, String room_id){//Updates an existing record in the Occupant Table
 		String url= "jdbc:sqlite:Room_Information.db";
 		try(Connection conn = DriverManager.getConnection(url);
 			Statement stmt = conn.createStatement()){
 			String query = "UPDATE Occupant set idNum="+new_p.getIdNum()
 						+",fname='"+new_p.getfName()+"'"
+						+",lname='"+new_p.getlName()+"'"
 						+",phoneNumber='"+new_p.getPhoneNumber()+"'"
 						+",email='"+new_p.getEmail()+"'"
-						+",room_id="+"1 WHERE idNum="+id; //Missing Room ID Parameter
-			stmt.execute(query);
-			System.out.println("Update Successful");
+						+",room_id="+room_id+" WHERE idNum="+id; //Missing Room ID Parameter
+			int num =stmt.executeUpdate(query);
+			if(num==0) {
+				System.out.println("No record with id number "+id+" was found.");
+			}else{
+				System.out.println("Update Successful");
+			}
+			
 		}catch (SQLException e) {
 			if (e.getMessage().contains("UNIQUE constraint failed")) {
                 System.out.println("Error: Duplicate ID Number. Entry already exists.");
@@ -103,6 +114,63 @@ public class DataManager {
 			
                 System.out.println("Database error: " + e.getMessage());
 		}
+	}
+	
+	public static void insertFurniture(Furniture f, String r_id){ //Inserts new furniture information into the occupant table
+		String url= "jdbc:sqlite:Room_Information.db";
+		try(Connection conn = DriverManager.getConnection(url);
+			Statement stmt = conn.createStatement()){
+			String query = "INSERT INTO Furniture (type,state,room_id) VALUES ("+f.getType()+",'"+f.getState()+"','"+r_id+ ")"; //Missing Room ID Parameter
+			stmt.execute(query);
+			System.out.println("Insert Successful");
+		}catch (SQLException e) {
+			if (e.getMessage().contains("UNIQUE constraint failed")) {
+                System.out.println("Error: Duplicate ID Number. Entry already exists.");
+            } else {
+                System.out.println("Database error: " + e.getMessage());
+            }
+		}
+	}
+	
+	public static void updateFurniture(long id, Furniture f, String room_id){//Updates an existing record in the Occupant Table
+		String url= "jdbc:sqlite:Room_Information.db";
+		try(Connection conn = DriverManager.getConnection(url);
+			Statement stmt = conn.createStatement()){
+			String query = "UPDATE Furniture set idNum="
+						+",type='"+f.getType()+"'"
+						+",state='"+f.getState()+"'"
+						+",room_id="+room_id+" WHERE idNum="+id; //Missing Room ID Parameter
+			int num =stmt.executeUpdate(query);
+			if(num==0) {
+				System.out.println("No record with id number "+id+" was found.");
+			}else{
+				System.out.println("Update Successful");
+			}
+			
+		}catch (SQLException e) {
+			if (e.getMessage().contains("UNIQUE constraint failed")) {
+                System.out.println("Error: Duplicate ID Number. Entry already exists.");
+            } else {
+                System.out.println("Database error: " + e.getMessage());
+            }
+		}
+		
+	}
+	
+	public static void clearDatabase() {
+		String url= "jdbc:sqlite:Room_Information.db";
+		try(Connection conn = DriverManager.getConnection(url);
+			Statement stmt = conn.createStatement()){
+			String query = "DELETE FROM Furniture; ";
+			stmt.execute(query);
+			query = "DELETE FROM Occupant;";
+			stmt.execute(query);
+			query = "DELETE FROM Rooms;";
+			stmt.execute(query);
+		}catch (SQLException e) {
+			System.out.println("Database error: " + e.getMessage());
+		}
+		
 	}
 	
 	
--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,6 @@
 
 public class Main { //Small Demonstration of database methods 
 	public static void main(String[] args){
-		DataManager.createDatabase();
-		DataManager.showOccupants(); //Shows which occupants are currently stored in the database
-		Occupant pete = new Occupant("Peter", "Morg", 620153044, "(876)123-2344", "Petersalrgiht@gmail.com");
-		DataManager.insertOccupant(pete);//Confirming that duplicates arent allowed
-		Occupant pete2 = new Occupant("Peter", "Morg", 620153032, "(876)123-2344", "Petersaintalright@gmail.com");
-		DataManager.updateOccupant(620153044,pete2);//Altering 
-		DataManager.showOccupants();
-	}
+	
 }
+
