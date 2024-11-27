6dc04461d9b2b490f3f86436af6e15fab9cef33c
willando-blair <willando.blair@mymona.uwi.edu>
2024-11-27 14:45:46 -0500

    Added sort dropdown menu

--- src/RoomDisplayUI.java
+++ src/RoomDisplayUI.java
@@ -3,10 +3,10 @@ import javax.swing.table.DefaultTableModel;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.util.ArrayList;
 
 public class RoomDisplayUI extends JFrame implements ActionListener {
-    private JPanel disPanel;
-    private JButton btHome, btSortRNum, btSortBlock, btSortCondition;
+    private final JButton btHome;
     private DefaultTableModel tableModel;
 
 
@@ -18,14 +18,16 @@ public class RoomDisplayUI extends JFrame implements ActionListener {
         setLayout(new BorderLayout());
 
         JPanel HEADERPANEL = new JPanel(new FlowLayout());
+        HEADERPANEL.setBackground(Color.black);
         HEADERPANEL.setPreferredSize(new Dimension(700, 40));
         add(HEADERPANEL, BorderLayout.NORTH);
 
         JPanel FOOTERPANEL = new JPanel(new FlowLayout());
+        FOOTERPANEL.setBackground(Color.black);
         FOOTERPANEL.setPreferredSize(new Dimension(700, 40));
         add(FOOTERPANEL, BorderLayout.SOUTH);
 
-        disPanel = new JPanel();
+        JPanel disPanel = new JPanel();
         disPanel.setVisible(true);
         disPanel.setPreferredSize(new Dimension(800, 700));
         add(disPanel, BorderLayout.CENTER);
@@ -33,22 +35,23 @@ public class RoomDisplayUI extends JFrame implements ActionListener {
         String[] columnNames = {"Room Number", "Occupant"};
 
         btHome = new JButton("Home");
-        btHome.setFocusable(false);
+        btHome.setFocusable(true);
         btHome.addActionListener(this);
         HEADERPANEL.add(btHome);
 
-        btSortRNum = new JButton("Sort by Room Number");
-        btSortRNum.setFocusable(true);
-        btSortRNum.addActionListener(this);
+        JLabel lbSort = new JLabel("Sort by: ");
+        lbSort.setForeground(Color.white);
+        HEADERPANEL.add(lbSort);
 
-        btSortBlock = new JButton("Sort by Block");
-        btSortBlock.setFocusable(true);
-        btSortBlock.addActionListener(this);
+        String[] sortOptions = {"Christian Name (Ascending)", "Christian Name (Descending)", "Surname (Ascending)",
+                "Surname (Descending)", "Room Number (Ascending)", "Room Number (Descending)",
+                "Condition (Ascending)", "Condition (Descending)", "Block (Ascending)", "Block (Descending)"};
+        JComboBox cmbSortOptions = new JComboBox(sortOptions);
+        cmbSortOptions.setEditable(false);
+        cmbSortOptions.addActionListener(this);
+        HEADERPANEL.add(cmbSortOptions);
 
-        btSortCondition = new JButton("Sort by Furniture Condition");
-        btSortCondition.setFocusable(true);
-        btSortCondition.addActionListener(this);
-
+        ////////////////////////////////////////////////////////////////////////////////////////////////////////////////
         getContentPane().add(HEADERPANEL, BorderLayout.NORTH);
         getContentPane().add(disPanel, BorderLayout.CENTER);
         getContentPane().add(FOOTERPANEL, BorderLayout.SOUTH);
@@ -57,20 +60,16 @@ public class RoomDisplayUI extends JFrame implements ActionListener {
         setVisible(true);
     }
 
-    public static void main(String [] args) {
-        new RoomDisplayUI();
-    }
-
     @Override
     public void actionPerformed(ActionEvent e) {
         if (e.getSource() == btHome) {
-
-        } else if (e.getSource() == btSortBlock) {
-
-        } else if (e.getSource() == btSortCondition) {
-
-        } else if (e.getSource() == btSortRNum) {
-
+            System.out.println("Home pressed");
         }
     }
+
+    public static void main(String [] args) {
+        new RoomDisplayUI();
+    }
 }
+
+
