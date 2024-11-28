c4df71b1c2ca06f6e1516216983241a4ae91e378
vdrews <vanessa.andrews876@gmail.com>
2024-11-27 20:49:45 -0500

    Updated ReportGenerator

---
+++ src/ReportGenerator.java
@@ -0,0 +1,82 @@
+public  class ReportGenerator{
+    private TaylorAdmin TaylorHall;
+
+
+    
+        
+    
+
+
+
+    public static void main(String[] args) {
+        // Example login attempts
+        }
+
+
+}
+
+
+
+
+         /*
+        public class ReportGenerator(){}
+        
+        public class ReportGenerator(TaylorAdmin TaylorHall){
+            this.TaylorHall = TaylorHall;
+        }
+        
+        public int blockCount(){//tells the number of blocks
+           return TaylorHall.getnumBlocks();
+        }
+        
+        /*public String listAllBlocks(){//list all the blocks
+            for(Block block: TaylorHall.getAllBlocks()){
+                return TaylorHall.toString();
+            
+            }
+        }*/
+        
+        
+        //tells the number of furniture in ach room 
+        //tells state of each furniture in the room
+        // tells number of occupant in room
+        
+        
+         
+        //  int count = 0;
+        //  for(Blocks block: TaylorAdmin.getBlock()){
+            
+        
+        
+        
+        //  }
+        
+        
+        
+        
+        
+        
+        
+        /* 
+        public static void main(String[] args) {
+            // Example login attempts
+            public static void main(String[] args) {
+                // Example login attempts
+             
+                 // Should return 1 (success)
+                
+                TaylorAdmin TaylorHall = new TaylorAdmin(DataManager.loadData());
+                  ///testing 
+            
+                //sys.out TaylorHall.blockCount();
+                System.out.println(TaylorHall.blockCount());
+                TaylorAdmin.listAllBlocks();
+           
+            }
+        
+          
+        
+            */
+        
+        
+        
