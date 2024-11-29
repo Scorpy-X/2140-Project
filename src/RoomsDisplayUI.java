6a6dd4909596353d6538a28d1b1edaf2d8fdfbdd
willando-blair <willando.blair@mymona.uwi.edu>
2024-11-28 21:29:14 -0500

    *removed additional constructor from Rooms Display UI

--- src/RoomsDisplayUI.java
+++ src/RoomsDisplayUI.java
@@ -11,7 +11,6 @@ public class RoomsDisplayUI extends JFrame implements ActionListener {
     private int accessLevel;
     private TaylorAdmin database;
 
-
     public RoomsDisplayUI(TaylorAdmin db, int accessLevel){
         setTitle("Room Display");
         this.database = db;
@@ -63,64 +62,6 @@ public class RoomsDisplayUI extends JFrame implements ActionListener {
             allRooms.addAll(b.getRooms());
         }
         loadRoomsTable(allRooms);
-
-        ////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-        getContentPane().add(HEADERPANEL, BorderLayout.NORTH);
-        getContentPane().add(disPanel, BorderLayout.CENTER);
-        getContentPane().add(FOOTERPANEL, BorderLayout.SOUTH);
-
-        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        setVisible(true);
-    }
-
-    public RoomsDisplayUI(Block block, int accessLevel){
-        this.accessLevel = accessLevel;
-        setTitle("Room Display - Block");
-        setSize(Toolkit.getDefaultToolkit().getScreenSize());
-        setExtendedState(JFrame.MAXIMIZED_BOTH);
-        setExtendedState(JFrame.MAXIMIZED_BOTH);
-        setLayout(new BorderLayout());
-
-        JPanel HEADERPANEL = new JPanel(new FlowLayout());
-        HEADERPANEL.setBackground(new Color(40, 100, 220));
-        HEADERPANEL.setPreferredSize(new Dimension(700, 40));
-        add(HEADERPANEL, BorderLayout.NORTH);
-
-        JPanel FOOTERPANEL = new JPanel(new FlowLayout());
-        FOOTERPANEL.setBackground(new Color(40, 100, 220));
-        FOOTERPANEL.setPreferredSize(new Dimension(700, 40));
-        add(FOOTERPANEL, BorderLayout.SOUTH);
-
-        JPanel disPanel = new JPanel();
-        disPanel.setVisible(true);
-        disPanel.setPreferredSize(new Dimension(800, 700));
-        add(disPanel, BorderLayout.CENTER);
-
-        btHome = new JButton("Home");
-        btHome.setFocusable(true);
-        btHome.addActionListener(this);
-        FOOTERPANEL.add(btHome);
-
-        JLabel lbSort = new JLabel("Sort by: ");
-        lbSort.setForeground(Color.white);
-        HEADERPANEL.add(lbSort);
-
-        String[] sortOptions = {"First Name (Ascending)", "First Name (Descending)", "Surname (Ascending)",
-                "Surname (Descending)", "Room Number (Ascending)", "Room Number (Descending)",
-                "Condition (Ascending)", "Condition (Descending)", "Block (Ascending)", "Block (Descending)"};
-        JComboBox cmbSortOptions = new JComboBox(sortOptions);
-        cmbSortOptions.setEditable(false);
-        cmbSortOptions.addActionListener(this);
-        HEADERPANEL.add(cmbSortOptions);
-
-        String[] columnNames = {"Room Number", "Room Type", "Occupant's First Name", "Occupant's Surname"};
-        tableModel = new DefaultTableModel(columnNames, 0);
-        JTable rTable = new JTable(tableModel);
-        disPanel.add(rTable);
-        JScrollPane scrollPane = new JScrollPane(rTable);
-        add(scrollPane);
-        ArrayList<Room> rooms = new ArrayList<>(block.getRooms());
-        loadRoomsTable(rooms);
 
         ////////////////////////////////////////////////////////////////////////////////////////////////////////////////
         getContentPane().add(HEADERPANEL, BorderLayout.NORTH);
